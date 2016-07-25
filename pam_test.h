c29ecd737114a793175e702cd4fd9096360772a4
wangkeyi <keyi.wang@i-soft.com.cn>
2016-07-25 09:26:03 +0800

    creat project

---
+++ pam_test.h
@@ -0,0 +1,14 @@
+#ifndef __PAM_TEST_H
+#define __PAM_TEST_H
+#define MODULE_NAME "pam_my"
+#define SAMPLE_PROMPT "Extra Password for root:"
+#define PAM_DEBUG_ARG      1
+
+#define DPRINT if (ctrl & PAM_DEBUG_ARG) my_syslog
+
+#define PAM_RET_CHECK(ret) if(PAM_SUCCESS != ret)  {return ret; }
+
+int my_converse (pam_handle_t * pamh, int msg_style, char *message,char **password);
+void my_pam_free (pam_handle_t * pamh, void *pbuf, int error_status);
+
+#endif
---
+++ skf_test.h
@@ -0,0 +1,14 @@
+#ifndef __SKF_TEST_H
+#define __SKF_TEST_H
+#include "SKFAPI.h"
+#include <security/pam_modules.h>
+#include <security/pam_appl.h>
+
+
+BOOL Test_ConnectDev(pam_handle_t *pamh,HANDLE *g_hDev);
+BOOL Test_GetDevInfo_serialnu(HANDLE g_hDev, DEVINFO **devinfo);
+BOOL Test_DevAuth(HANDLE g_hDev, HANDLE *m_hSessionKey);
+BOOL Test_OpenApplication(HANDLE g_hDev, HANDLE *g_hApp);
+BOOL Test_VerifyPIN(pam_handle_t *pamh,HANDLE g_hApp);
+
+#endif
